66e763a2f2820a4e43a0386dc89099510efecacd
Luiz-F3lipe <luizf3lipe.dev@gmail.com>
2025-09-02 22:59:15 -0300

    initial project

---
+++ app/src/main/java/com/luizf3lipe/weather/screens/HomeScreen.kt
@@ -0,0 +1,129 @@
+package com.luizf3lipe.weather.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.luizf3lipe.weather.R
+import com.luizf3lipe.weather.ui.theme.BlueLight
+import com.luizf3lipe.weather.ui.theme.Gray100
+import com.luizf3lipe.weather.ui.theme.Gray200
+import com.luizf3lipe.weather.ui.theme.Gray300
+import com.luizf3lipe.weather.ui.theme.Gray400
+import com.luizf3lipe.weather.ui.theme.Gray600
+import com.luizf3lipe.weather.ui.theme.Gray800
+import com.luizf3lipe.weather.ui.theme.Typography
+import com.luizf3lipe.weather.ui.theme.White
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = "background image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "logo image",
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 32.dp),
+            contentScale = ContentScale.FillWidth
+        )
+
+        Column(
+            modifier = modifier
+                .align(Alignment.Center)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
+                Text(
+                    text = "Boas Vindas ao ",
+                    style = Typography.headlineSmall,
+                    color = White
+                )
+
+                Text(
+                    text = "IWeather",
+                    style = Typography.headlineSmall,
+                    color = BlueLight
+                )
+            }
+            Text(
+                text = "Escolha um local para ver a previsão do tempo",
+                style = Typography.bodyMedium,
+                color = Gray200
+            )
+
+            SearchInput()
+        }
+    }
+}
+
+@Composable
+fun SearchInput() {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        placeholder = {
+            Text(
+                text = "Buscar local",
+                color = Gray400
+            )
+        },
+        singleLine = true,
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Gray600,
+            focusedTextColor = Gray400,
+            unfocusedTextColor = Gray400,
+            unfocusedContainerColor = Gray600,
+            disabledContainerColor = Gray600,
+            cursorColor = BlueLight,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp, vertical = 24.dp)
+            .height(56.dp)
+            .clip(RoundedCornerShape(12.dp))
+    )
+}
+
+@Preview
+@Composable
+private fun HomeScreenPrev() {
+    HomeScreen(modifier = Modifier.fillMaxSize())
+}
---
+++ app/src/main/java/com/luizf3lipe/weather/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.luizf3lipe.weather.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// COLORS APP
+val BlueLight = Color(0xFF8FB2F5)
+
+val Gray900 = Color(0xFF13131A)
+val Gray800 = Color(0xFF16161F)
+val Gray700 = Color(0xFF1C1C27)
+val Gray600 = Color(0xFF22222F)
+val Gray500 = Color(0xFF3B3B54)
+val Gray400 = Color(0xFF7F7F98)
+val Gray300 = Color(0xFFABABC4)
+val Gray200 = Color(0xFFBFBFD4)
+val Gray100 = Color(0xFFFAFAFA)
+val White   = Color(0xFFFFFFFF)
